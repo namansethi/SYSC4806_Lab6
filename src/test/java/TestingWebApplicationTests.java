183942ed34f35d66116fd1f8c99af9f9390fd7fc
Naman Sethi <namansethi@cmail.carleton.ca>
2022-03-04 12:56:35 -0500

    updated.

--- src/test/java/TestingWebApplicationTests.java
+++ src/test/java/TestingWebApplicationTests.java
@@ -1,8 +1,10 @@
+import Spring.AccessingDataJpaApplication;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
-@SpringBootTest
+@SpringBootTest(classes = AccessingDataJpaApplication.class)
 public class TestingWebApplicationTests {
 
     @Test
